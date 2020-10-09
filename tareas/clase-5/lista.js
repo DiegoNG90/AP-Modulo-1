33fc981ffd86f962b371aaee18f89aaf6797bb42
DiegoNG90 <63425447+DiegoNG90@users.noreply.github.com>
2020-10-09 02:53:49 -0300

    Work on 4. task assignment
    In order to find the most repeated number on the list, I did:
    1) A function that takes a nodelist and returns an array (nodeToArray function)
    
    2) another function that takes an array as an argument and returns the repeated numbers in an array(numerosRepetidosEnArray function). For this task, it works fairly (cause only number 15 is repeated), but this is not optimal for bigger arrays, which can contain more than one number that may be repeating.
    
    3) For optimization purposes, I created a third function (cantidadDeRepeticionesEnUnaArrayPorElemento) that takes an array as parameter and returns an object with the number -as property- and the times that it's repeated -value-.
    
    STILL ON WORK: create a 4th Function that takes that object (from previous function) and returns the most repeated number.

--- tareas/clase-5/lista.js
+++ tareas/clase-5/lista.js
@@ -22,6 +22,59 @@ let promedio = (suma/($nodoLista.length));
 //     sum += item;
 // })
 
+function nodeToArray(nodeList){
+    const newArray = [];
+    nodeList.forEach(element => {
+        newArray.push(Number(element.innerText));
+    });
+    return newArray;
+}
+const edadesArr = nodeToArray($nodoLista)
+
+//4
+
+
+function numerosRepetidosEnArray(array){
+    let numerosrepetidos = [];
+    for(let i=0; i < array.length;i++){
+        for(let j=0; j < array.length;j++){
+            if (j!==i && array[i]==array[j]){
+                numerosrepetidos.push(array[i])
+            }
+        }
+    }return numerosrepetidos
+}
+console.log(numerosRepetidosEnArray(edadesArr));
+let edadRepetida = numerosRepetidosEnArray(edadesArr)[0];
+
+//edadesArr     0  1   2  3   4   5
+//(6)         [22, 5, 15, 7, 42, 15]
+function cantidadDeRepeticionesEnUnaArrayPorElemento (array){
+    let objetoNum = {};
+    let contador = 1;
+    for (let i = 0; i < array.length; i++) {
+        if (objetoNum[i] === undefined) {
+            if (!(array[i] in objetoNum)) {
+                contador = 1;
+                objetoNum[array[i]] = contador;            
+            }else{
+                contador = objetoNum[array[i]] + 1;
+                objetoNum[array[i]] = contador; 
+            }
+        }else{
+            contador = objetoNum[array[i]] + 1;
+            objetoNum[array[i]] = contador;   
+        }
+    }return objetoNum;
+}
+//Pruebas:                 
+let edadesArray2 = [22, 5, 15, 7, 42, 15, 15, 15, 15, 9, 11, 9 , 11 , 11 , 15]
+
+let arr1 = [0,1,1,2,3,4,4,5,5,5,6,7,8,8];
+console.log( cantidadDeRepeticionesEnUnaArrayPorElemento(arr1));
+console.log( cantidadDeRepeticionesEnUnaArrayPorElemento(edadesArr));
+console.log( cantidadDeRepeticionesEnUnaArrayPorElemento(edadesArray2));
+
 
 console.log("Promedio " + promedio.toFixed(2));
 document.querySelector('#promedio').innerText = `El promedio es ${promedio.toFixed(2)}`
@@ -29,5 +82,10 @@ console.log("Numero mas grande " + Math.max(...lista));
 document.querySelector('#grande').innerText = `El numero más grande es ${Math.max(...lista)}`
 console.log("Numero mas pequeño " +Math.min(...lista));
 document.querySelector('#pequeño').innerText = `El numero más pequeño es ${Math.min(...lista)}`
-document.querySelector('#repetido').innerText = `El numero más veces repetido es: no sé calcular ésto todavía`
+document.querySelector('#repetido').innerText = `El numero más veces repetido es: ${edadRepetida}`
+console.log(`Numero repetido: ${edadRepetida}`);
+
+
+
+
 
